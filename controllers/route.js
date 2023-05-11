bbf81288cae3bad864f90ff30e9c779fb9447b6e
yxmxshd0 <yxmxsh1do@gmail.com>
2023-05-12 00:04:10 +0300

    Final logic of server work

--- controllers/route.js
+++ controllers/route.js
@@ -3,32 +3,54 @@ const { Validation } = require('../middlewares/middleware');
 var router = express.Router();
 
 
-
-
-
 //#заполнил для тестов
 
 let trips = [
-    {
-        id: "2",
-        destination:"string",
-        spotsleft:"26",
-		date: "20-02-2003"
-    },
-    {
-        id: "1",
-        destination: "string",
-        spotsleft: "20",
-        date: "20-02-2003"
-    }
+    [
+        {
+			id: "2",
+			destination:"string",
+			spotsleft:"3",
+			date: "20-02-2003"
+		},
 
+		[
+			{
+				"name":"Roman"
+			}
+		]
+
+	],
+	[
+        {
+			id: "1",
+			destination:"string",
+			spotsleft:"20",
+			date: "20-02-2003"
+		},
+
+		[
+			{
+				name:"Dead Roman"
+			}
+		]
+
+	]
 ];
 
 
-router.get('/trips', (req,res)=>
+router.get('/trips/:id', (req,res)=>
 {
-    
-    res.send(trips);
+    let id = req.params.id;
+	for (let x = 0; x<trips.length;x++)
+	{
+		if(trips[x][0].id===id)
+		{
+			currTrip = trips[x]
+		}
+		
+	}
+    res.send(currTrip);
 
 })
 
@@ -36,39 +58,48 @@ router.get('/trips', (req,res)=>
 router.post(`/trips/:id/passengers`, Validation,(req,res)=>
 {
     let id = req.params.id
+
+
+	let currTrip 
+
+	for (let x = 0; x<trips.length;x++)
+	{
+		if(trips[x][0].id===id)
+		{
+			currTrip = trips[x]
+		}
+		
+	}
 	
-	let currTrip = trips.filter(trip => trip.id === id);
 
     let dataPass = req.body
 
+	console.log(currTrip)
+
 	console.log(currTrip[0].spotsleft);
 
+	console.log(currTrip[1])
 
-    if (currTrip[0].spotsleft>0)
-    {
-		currTrip[0].spotsleft-=1;
-        currTrip.push(dataPass);
-
-		const currTripIndex = trips.findIndex(trip => trip.id === id)
-
-        if (currTripIndex>=0)
-        {
-            trips[currTripIndex]=currTrip; //перезапись 
-        }
-        else
-        {
-            res
-                .send(400, `<h1>Путешествие не зарегистрировано</h1>`)
-        }
-    }
+	if (currTrip)
+	{
+		if (currTrip[0].spotsleft>0)
+		{
+			currTrip[0].spotsleft-=1;
+			currTrip[1].push(dataPass)
+		}
+		else 
+		{
+			res.send(400, `Нет свободных мест`)
+		}
+	}
     else
         {
-            res.send (400, `Больше мест нет`)
+			res.send(400, `<h1>Путешествие не зарегистрировано</h1>`)
         }
 
     res.send(trips)
     
-} )
+} );
 
 
 router.post('/trips', Validation,  (req,res)=>
